a7b08a3863d10e416cd22cb2d3a559347aa5b3d0
Peter Hahn Boelt <1081439@ucn.dk>
2020-10-27 09:14:31 +0100

    Test Peter

--- WebApplication/Models/ErrorViewModel.cs
+++ WebApplication/Models/ErrorViewModel.cs
@@ -5,5 +5,6 @@ namespace WebApplication.Models
         public string RequestId { get; set; }
 
         public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+        //This is a test - Peter
     }
 }
