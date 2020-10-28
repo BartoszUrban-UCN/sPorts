4984f894d42910a9b227f79da017fabd1b4106e0
Zaharia Horatau-Hostiuc <1081482@ucn.dk>
2020-10-28 13:37:23 +0100

    Reverted the role pattern

--- WebApplication/Models/MarinaOwner.cs
+++ WebApplication/Models/MarinaOwner.cs
@@ -1,19 +1,9 @@
 ﻿using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
 
 namespace WebApplication.Models
 {
     public class MarinaOwner
     {
-        [Key]
-        public int MarinaOwnerId { get; set; }
-
-        [ForeignKey("Person")]
-        public int PersonId { get; set; }
-
-        public Person Person { get; set; }
-
         public List<Marina> Marinas { get; set; }
     }
 }
