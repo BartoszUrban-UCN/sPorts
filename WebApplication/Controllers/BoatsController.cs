658ee3ff5ec6b9602f8b08f420f76dd488cf5514
Zaharia Horatau-Hostiuc <1081482@ucn.dk>
2020-11-02 16:45:48 +0100

    Initial Boat CRUD Logic

---
+++ WebApplication/Controllers/BoatsController.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using WebApplication.Data;
+using WebApplication.Models;
+
+namespace WebApplication.Controllers
+{
+    public class BoatsController : Controller
+    {
+        private readonly SportsContext _context;
+
+        public BoatsController(SportsContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Boats
+        public async Task<IActionResult> Index()
+        {
+            var sportsContext = _context.Boats.Include(b => b.BoatOwner);
+            return View(await sportsContext.ToListAsync());
+        }
+
+        // GET: Boats/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var boat = await _context.Boats
+                .Include(b => b.BoatOwner)
+                .FirstOrDefaultAsync(m => m.BoatId == id);
+            if (boat == null)
+            {
+                return NotFound();
+            }
+
+            return View(boat);
+        }
+
+        // GET: Boats/Create
+        public IActionResult Create()
+        {
+            ViewData["BoatOwnerId"] = new SelectList(_context.BoatOwners, "BoatOwnerId", "BoatOwnerId");
+            return View();
+        }
+
+        // POST: Boats/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("BoatId,Name,RegistrationNo,Width,Length,Depth,Type,BoatOwnerId")] Boat boat)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(boat);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["BoatOwnerId"] = new SelectList(_context.BoatOwners, "BoatOwnerId", "BoatOwnerId", boat.BoatOwnerId);
+            return View(boat);
+        }
+
+        // GET: Boats/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var boat = await _context.Boats.FindAsync(id);
+            if (boat == null)
+            {
+                return NotFound();
+            }
+            ViewData["BoatOwnerId"] = new SelectList(_context.BoatOwners, "BoatOwnerId", "BoatOwnerId", boat.BoatOwnerId);
+            return View(boat);
+        }
+
+        // POST: Boats/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("BoatId,Name,RegistrationNo,Width,Length,Depth,Type,BoatOwnerId")] Boat boat)
+        {
+            if (id != boat.BoatId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(boat);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!BoatExists(boat.BoatId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["BoatOwnerId"] = new SelectList(_context.BoatOwners, "BoatOwnerId", "BoatOwnerId", boat.BoatOwnerId);
+            return View(boat);
+        }
+
+        // GET: Boats/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var boat = await _context.Boats
+                .Include(b => b.BoatOwner)
+                .FirstOrDefaultAsync(m => m.BoatId == id);
+            if (boat == null)
+            {
+                return NotFound();
+            }
+
+            return View(boat);
+        }
+
+        // POST: Boats/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var boat = await _context.Boats.FindAsync(id);
+            _context.Boats.Remove(boat);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool BoatExists(int id)
+        {
+            return _context.Boats.Any(e => e.BoatId == id);
+        }
+    }
+}
