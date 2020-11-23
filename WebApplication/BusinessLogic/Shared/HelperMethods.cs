cb4d12954dd252eccbc24745ffee0c5409a218d0
Zaharia Horatau-Hostiuc <1081482@ucn.dk>
2020-11-23 13:22:09 +0100

    Booking Map

---
+++ WebApplication/BusinessLogic/Shared/HelperMethods.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Html;
+using Newtonsoft.Json;
+
+namespace WebApplication.BusinessLogic.Shared
+{
+    public class HelperMethods
+    {
+        public static string Serialize(object @object)
+        {
+            return JsonConvert.SerializeObject(@object, Formatting.Indented, new JsonSerializerSettings
+                {
+                    ReferenceLoopHandling = ReferenceLoopHandling.Ignore
+                });
+        }
+    }
+}
---
+++ WebApplication/wwwroot/js/leaflet_methods.js
@@ -0,0 +1,15 @@
+﻿function AddMarinaOnMap(marina) {
+    var marinaPopup = `\
+        <div class="text-center" style="width: 100px">\
+            <p style="font-weight: bold">${marina.Name}</p>\
+            <a href="/Marina/Details/${marina.MarinaId}" class="btn btn-primary text-white">Details</a>\
+        </div>\
+    `;
+
+    L.circle([marina.Location.Latitude, marina.Location.Longitude], {
+        color: 'pink',
+        fillColor: '#f03',
+        fillOpacity: 0.5,
+        radius: (marina.Location.Radius * 1000),
+    }).bindPopup(marinaPopup).openTooltip().addTo(mymap);
+}
