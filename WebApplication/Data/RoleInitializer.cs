e2fc40af3dcae979829f23d596bb9eb75cf814e3
Zaharia Horatau-Hostiuc <1081482@ucn.dk>
2020-12-06 14:11:56 +0100

    Added boats menu on the navbar. Small refactorings in RoleInitializer.

--- WebApplication/Data/RoleInitializer.cs
+++ WebApplication/Data/RoleInitializer.cs
@@ -4,14 +4,19 @@ using System;
 using System.Linq;
 using System.Threading.Tasks;
 using WebApplication.Models;
+using WebApplication.Authorization;
 
 namespace WebApplication.Data
 {
     public static class RoleInitializer
     {
-        // These are the role names we want to always have in the database at
-        // the start
-        private static readonly string[] RoleNames = { "Manager", "BoatOwner", "MarinaOwner" };
+        // These are the role names we want to always have in the database at the start
+        private static readonly string[] RoleNames = {
+            RoleName.BoatOwner,
+            RoleName.MarinaOwner,
+            RoleName.Manager,
+            RoleName.Administrator
+        };
 
         // Seed roles in the db if they don't exist
         public static async Task SeedRoles(IServiceProvider serviceProvider, SportsContext context)
@@ -24,7 +29,7 @@ namespace WebApplication.Data
                 {
                     if (!await roleManager.RoleExistsAsync(role))
                     {
-                        await roleManager.CreateAsync(new WebApplication.Models.Role(role));
+                        await roleManager.CreateAsync(new Role(role));
                     }
                 }
             }
