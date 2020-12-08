a4babe44809980d27b0862bb87620e520538584f
Zaharia Horatau-Hostiuc <1081482@ucn.dk>
2020-12-08 09:50:37 +0100

    "Mindbreaking" changes for Mogens when it comes to maps.

--- WebApplication/wwwroot/js/leaflet/leaflet_map.js
+++ WebApplication/wwwroot/js/leaflet/leaflet_map.js
@@ -9,7 +9,7 @@ function initMap(mapid) {
 	L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
 		attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
 		maxZoom: 18,
-		id: 'mapbox/streets-v11',
+		id: 'mapbox/satellite-streets-v11',
 		tileSize: 512,
 		zoomOffset: -1,
 		accessToken: 'pk.eyJ1IjoiemFja2Zyb3N0IiwiYSI6ImNraGRncmF2dTA1cDcyc2w2dGhua3RwOHoifQ.NKvsgUWbNOT14E-72mHn0w'
