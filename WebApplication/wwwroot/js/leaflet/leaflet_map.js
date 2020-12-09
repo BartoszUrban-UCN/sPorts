f513303adb5eb3be8120e1bc4dfff600505abff0
Zaharia Horatau-Hostiuc <1081482@ucn.dk>
2020-12-09 19:37:24 +0100

    Small changes for zoom level Leaflet.

--- WebApplication/wwwroot/js/leaflet/leaflet_map.js
+++ WebApplication/wwwroot/js/leaflet/leaflet_map.js
@@ -4,11 +4,12 @@
  * @returns {L.Map} - leaflet map object
  */
 function initMap(mapid) {
-	var mymap = L.map(`${mapid}`)
+	var mymap = L.map(`${mapid}`);
 
 	L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
 		attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-		maxZoom: 18,
+		minZoom: 4,
+		maxZoom: 21,
 		id: 'mapbox/satellite-streets-v11',
 		tileSize: 512,
 		zoomOffset: -1,
